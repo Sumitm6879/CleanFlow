515f670ccb1f9fd6526b5c122b1bc2b47780e88b
Sumitm6879 <sm78810@gmail.com>
2025-08-10 04:23:15 +0530

    last one

--- client/lib/supabase.ts
+++ client/lib/supabase.ts
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://vmjicjqqgxpnoxzqwolz.supabase.co'
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtamljanFxZ3hwbm94enF3b2x6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ2ODIxMDYsImV4cCI6MjA3MDI1ODEwNn0.T5cfq69cbTdf9Wfzn2FYvyoRtvdT3qMo7w2kIb1gLeM'
-
+const supabaseUrl = process.env.VITE_SUPABASE_URL
+const supabaseAnonKey = process.env.VITE_SUPABASE_ANON
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 export type User = {
